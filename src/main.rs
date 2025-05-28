ee9df2f7d60e6e3b025bf2798306c3d4bdd7916b
dybucc <149513579+dybucc@users.noreply.github.com>
2025-05-28 19:00:15 +0200

    chore: wip

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #![expect(unused, reason = "Temporary allow during development.")]
 
+use std::sync::LazyLock;
+
 use clap::Parser;
 use color_eyre::{
     eyre::{bail, Result},
     install,
+    owo_colors::OwoColorize,
 };
+use fastrand::Rng;
 use ratatui::{
     crossterm::event::{read, Event, KeyCode},
     init,
     layout::Flex,
     prelude::{
-        Alignment, Buffer, Color, Constraint, Layout, Line, Modifier, Rect, Style, Stylize, Widget,
+        Alignment, Buffer, Color, Constraint, Layout, Line, Modifier, Rect, Style, Stylize, Text,
+        Widget,
     },
     restore,
     symbols::{block::FULL, DOT},
@@ -18,7 +23,8 @@ use ratatui::{
     DefaultTerminal,
 };
 use regex::Regex;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use ureq::agent;
 
 fn main() -> Result<()> {
     install()?;
@@ -38,8 +44,21 @@ struct Cli {
     /// This should be set through the command-line, the environment variable or the in-game menu.
     /// If not setting it through the in-game menu, one must use the name in the OpenRouter model
     /// page that appears right below the public-facing name.
-    #[arg(short, long, env = "OPENROUTER_MODEL", value_name = "MODEL_NAME")]
+    #[arg(
+        short,
+        long,
+        env = "OPENROUTER_MODEL",
+        value_name = "MODEL_NAME",
+        requires = "api_key"
+    )]
     model: Option<String>,
+    #[arg(
+        long,
+        env = "OPENROUTER_API_KEY",
+        value_name = "YOUR_API_KEY",
+        required = true
+    )]
+    api_key: String,
 }
 
 #[derive(PartialEq)]
@@ -88,6 +107,77 @@ enum EndMenuItem {
     Exit,
 }
 
+#[derive(Clone, Copy)]
+enum RandomResult {
+    Correct,
+    Incorrect,
+}
+
+static LLM_INPUT: LazyLock<&str> = LazyLock::new(|| {
+    "You will answer only to \"Correct\" or \"Incorrect.\" These correspond to either a\
+notification that a user got a number right in a number guessing game or not, respectively. Your\
+task is to, depending on whether you were notified they got it right, or not, to return a\
+cowboy-like answer to the user. Make it a short text. Include just your answer and nothing more.\
+Don't include emoji or otherwise non-verbal content."
+});
+
+#[derive(Serialize)]
+struct Request {
+    model: String,
+    messages: Vec<Message>,
+}
+
+impl Request {
+    fn new(model: String, result: RandomResult) -> Self {
+        match result {
+            RandomResult::Correct => Self {
+                model,
+                messages: vec![
+                    Message::new(Role::System, LLM_INPUT.to_string()),
+                    Message::new(Role::User, "Correct".to_owned()),
+                ],
+            },
+            RandomResult::Incorrect => Self {
+                model,
+                messages: vec![
+                    Message::new(Role::System, LLM_INPUT.to_string()),
+                    Message::new(Role::User, "Incorrect".to_owned()),
+                ],
+            },
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+struct Message {
+    role: Role,
+    content: String,
+}
+
+impl Message {
+    fn new(role: Role, content: String) -> Self {
+        Self { role, content }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Role {
+    System,
+    Assistant,
+    User,
+}
+
+#[derive(Deserialize)]
+struct ChatCompletionResponse {
+    choices: Vec<Choices>,
+}
+
+#[derive(Deserialize)]
+struct Choices {
+    message: Message,
+}
+
 #[derive(Deserialize)]
 struct Response {
     data: Vec<Data>,
@@ -104,6 +194,7 @@ struct App<'a> {
     score: u8,
     range_input: String,
     input: String,
+    result: Option<RandomResult>,
     model: String,
     models: Vec<String>,
     models_view: Vec<Line<'a>>,
@@ -114,6 +205,9 @@ struct App<'a> {
     ranged_re: Regex,
     input_re: Regex,
     extra_line_help: bool,
+    processing_request: bool,
+    rng: Rng,
+    chat_completion_output: String,
 }
 
 impl Default for App<'_> {
@@ -124,6 +218,7 @@ impl Default for App<'_> {
             exit: false,
             screen: Screen::MainMenu(MainMenuItem::Play),
             score: 0,
+            result: None,
             range_input: String::new(),
             input: String::new(),
             model: cli.model.unwrap_or("qwen/qwen3-32b:free".to_owned()),
@@ -132,10 +227,13 @@ impl Default for App<'_> {
             selectors_view: Vec::new(),
             model_view_selected: String::new(),
             model_view_offset: 0,
-            api_key: String::new(),
+            api_key: cli.api_key,
             ranged_re: Regex::new(r"\A\d+\.\.\d+\z").unwrap(),
             input_re: Regex::new(r"\A\d+\z").unwrap(),
             extra_line_help: false,
+            processing_request: false,
+            rng: Rng::new(),
+            chat_completion_output: String::new(),
         }
     }
 }
@@ -155,9 +253,10 @@ impl Widget for &mut App<'_> {
             Screen::InGame(s) => match s {
                 GameScreen::Game(s) => self.take_input(area, buf, s),
                 GameScreen::PauseMenu(s) => todo!(),
-                GameScreen::EndMenu(s) => todo!(),
+                GameScreen::EndMenu(s) => self.end_menu(area, buf, s),
             },
             Screen::ModelMenu => self.model_menu(area, buf),
+            _ => {}
         };
     }
 }
@@ -166,7 +265,7 @@ impl App<'_> {
     fn fetch_models(&mut self) {
         let response: Response = ureq::get("https://openrouter.ai/api/v1/models")
             .call()
-            .expect("request failed")
+            .expect("models request failed")
             .into_body()
             .read_json()
             .expect("json failed to parse");
@@ -206,6 +305,53 @@ impl App<'_> {
         }
     }
 
+    fn process_random(&mut self) {
+        let (start, end) = self
+            .range_input
+            .split_at(self.range_input.find("..").unwrap());
+        let end: String = end.chars().rev().collect();
+        let (end, _) = end.split_at(end.find("..").unwrap());
+
+        let start: usize = start.parse().unwrap();
+        let end: usize = end.parse().unwrap();
+        let guess: usize = self.input.parse().unwrap();
+
+        let random = self.rng.usize(start..=end);
+
+        if guess == random {
+            self.result = Some(RandomResult::Correct);
+        } else {
+            self.result = Some(RandomResult::Incorrect);
+        }
+    }
+
+    fn process_request(&mut self) {
+        let request_body = Request::new(self.model.clone(), self.result.unwrap());
+        let agent = agent();
+
+        loop {
+            match agent
+                .post("https://openrouter.ai/api/v1/chat/completions")
+                .header("Authorization", format!("Bearer {}", self.api_key))
+                .send_json(&request_body)
+            {
+                Ok(response) => {
+                    let response: ChatCompletionResponse =
+                        response.into_body().read_json().unwrap();
+                    let output = response.choices.last().unwrap().message.content.clone();
+
+                    if output.is_empty() {
+                        continue;
+                    } else {
+                        self.chat_completion_output = output;
+                        break;
+                    }
+                }
+                Err(_) => continue,
+            }
+        }
+    }
+
     fn run(&mut self, mut term: DefaultTerminal) -> Result<()> {
         while !self.exit {
             term.draw(|f| f.render_widget(&mut *self, f.area()))?;
@@ -215,54 +361,58 @@ impl App<'_> {
     }
 
     fn handle_events(&mut self) -> Result<()> {
+        if self.processing_request {
+            self.process_random();
+            self.process_request();
+            self.screen = Screen::InGame(GameScreen::EndMenu(EndMenuItem::Repeat));
+            self.processing_request = false;
+        }
+
         if let Event::Key(k) = read()? {
             match k.code {
                 KeyCode::Char(c)
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range))
+                        && !self.processing_request =>
                 {
                     self.range_input.push(c);
                 }
                 KeyCode::Char(c)
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input))
+                        && !self.processing_request =>
                 {
                     self.input.push(c);
                 }
                 KeyCode::Tab
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range))
+                        && !self.processing_request =>
                 {
                     self.screen = Screen::InGame(GameScreen::Game(GameItem::Input));
                 }
                 KeyCode::Tab
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input))
+                        && !self.processing_request =>
                 {
                     self.screen = Screen::InGame(GameScreen::Game(GameItem::Range));
                 }
                 KeyCode::Backspace
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range))
+                        && !self.processing_request =>
                 {
                     self.range_input.pop();
                 }
                 KeyCode::Backspace
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input)) =>
+                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input))
+                        && !self.processing_request =>
                 {
                     self.input.pop();
                 }
                 KeyCode::Enter
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Range)) =>
+                    if (self.screen == Screen::InGame(GameScreen::Game(GameItem::Range))
+                        || self.screen == Screen::InGame(GameScreen::Game(GameItem::Input)))
+                        && !self.processing_request =>
                 {
-                    // TODO process the request
                     if self.validate_input() {
-                        self.exit = true;
-                    } else {
-                        self.extra_line_help = true;
-                    }
-                }
-                KeyCode::Enter
-                    if self.screen == Screen::InGame(GameScreen::Game(GameItem::Input)) =>
-                {
-                    // TODO process the request
-                    if self.validate_input() {
-                        self.exit = true;
+                        self.processing_request = true;
                     } else {
                         self.extra_line_help = true;
                     }
@@ -295,6 +445,9 @@ impl App<'_> {
                             self.model_view_offset += 1;
                         }
                     }
+                    Screen::InGame(GameScreen::EndMenu(EndMenuItem::Repeat)) => {
+                        self.screen = Screen::InGame(GameScreen::EndMenu(EndMenuItem::Exit));
+                    }
                     _ => {}
                 },
                 KeyCode::Char('k') => match &self.screen {
@@ -324,6 +477,9 @@ impl App<'_> {
                             self.model_view_offset -= 1;
                         }
                     }
+                    Screen::InGame(GameScreen::EndMenu(EndMenuItem::Exit)) => {
+                        self.screen = Screen::InGame(GameScreen::EndMenu(EndMenuItem::Repeat));
+                    }
                     _ => {}
                 },
                 KeyCode::Char('l') => match &self.screen {
@@ -346,6 +502,13 @@ impl App<'_> {
                     }
                     Screen::ModelMenu => {
                         self.model = self.model_view_selected.clone();
+                    }
+                    Screen::InGame(GameScreen::EndMenu(EndMenuItem::Repeat)) => {
+                        // TODO repeat the game
+                        self.exit = true;
+                    }
+                    Screen::InGame(GameScreen::EndMenu(EndMenuItem::Exit)) => {
+                        self.exit = true;
                     }
                     _ => {}
                 },
@@ -580,15 +743,19 @@ impl App<'_> {
         if self.extra_line_help {
             let help_line = Block::new()
                 .title_top("Incorrect input")
-                .style(
-                    Style::default()
-                        .fg(Color::White)
-                        .add_modifier(Modifier::BOLD),
-                )
+                .style(Style::default().fg(Color::Red).add_modifier(Modifier::BOLD))
                 .title_alignment(Alignment::Center)
                 .borders(Borders::TOP);
 
             help_line.render(layout[2], buf);
+        } else if self.processing_request {
+            let processing_text = Block::new()
+                .title_top(format!("{DOT} Processing {DOT}"))
+                .title_alignment(Alignment::Center)
+                .style(Style::default().fg(Color::White))
+                .borders(Borders::TOP);
+
+            processing_text.render(layout[2], buf);
         }
 
         let ranged_input_space = ranged_input_block.inner(layout[0]);
@@ -613,5 +780,71 @@ impl App<'_> {
 
         ranged_input.render(ranged_input_space, buf);
         input.render(guess_input_space, buf);
+    }
+
+    fn end_menu(&self, area: Rect, buf: &mut Buffer, screen: &EndMenuItem) {
+        self.clear(area, buf);
+
+        let space = Layout::vertical([
+            Constraint::Percentage(40),
+            Constraint::Percentage(100),
+            Constraint::Percentage(40),
+        ])
+        .split(area)[1];
+        let space = Layout::horizontal([
+            Constraint::Percentage(40),
+            Constraint::Percentage(100),
+            Constraint::Percentage(40),
+        ])
+        .split(space)[1];
+
+        let layout = Layout::vertical([Constraint::Min(1), Constraint::Max(4)])
+            .flex(Flex::Center)
+            .split(space);
+
+        let result_block = Block::bordered()
+            .title_top({
+                match self.result.unwrap() {
+                    RandomResult::Correct => "Correct",
+                    RandomResult::Incorrect => "Incorrect",
+                }
+            })
+            .title_alignment(Alignment::Center)
+            .style(Style::default().fg(Color::Green))
+            .border_type(BorderType::Rounded);
+        let prompt_block = Block::new()
+            .title_top("Continue for another game?")
+            .title_bottom("(j) down / (k) up / (l) select")
+            .title_alignment(Alignment::Center)
+            .style(Color::Green)
+            .borders(Borders::TOP);
+
+        let result_space = result_block.inner(layout[0]);
+        let prompt_space = prompt_block.inner(layout[1]);
+
+        result_block.render(layout[0], buf);
+        prompt_block.render(layout[1], buf);
+
+        let result_text = Text::styled(self.chat_completion_output.clone(), Color::Green);
+        result_text.render(result_space, buf);
+
+        let prompt_layout =
+            Layout::vertical([Constraint::Max(1), Constraint::Max(1)]).split(prompt_space);
+        match screen {
+            EndMenuItem::Repeat => {
+                let yes = Line::styled("Yes", Style::default().bg(Color::Green).fg(Color::White));
+                let no = Line::styled("No", Color::Green);
+
+                yes.render(prompt_layout[0], buf);
+                no.render(prompt_layout[1], buf);
+            }
+            EndMenuItem::Exit => {
+                let yes = Line::styled("Yes", Color::Green);
+                let no = Line::styled("No", Style::default().bg(Color::Green).fg(Color::White));
+
+                yes.render(prompt_layout[0], buf);
+                no.render(prompt_layout[1], buf);
+            }
+        }
     }
 }
